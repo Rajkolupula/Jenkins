f70920995dda4c8b0bc4e52285613050b1ce3c71
Rajkolupula <rkolupula589@gmail.com>
2024-12-05 08:35:35 +0530

    Update python app.py

--- python app.py
+++ python app.py
@@ -1,9 +1,12 @@
+import json
 import boto3
-client = boto3.client('ec2')
-response = client.run_instances(
-     ImageId = 'ami-0614680123427b75e',
-     InstanceType = 't2.micro' ,
-     KeyName = 'mumbai-key',
-     MaxCount=1,
-     MinCount=1
+def lambda_handler(event, context):
+   client = boto3.client('ec2')
+   response = client.run_instances(
+      ImageId = 'ami-0614680123427b75e',
+      InstanceType = 't2.micro' ,
+      SubnetId = 'subnet-03b0f0d733b1cf64e',  
+      KeyName = 'mumbai-key',
+      MaxCount=1,
+      MinCount=1
 )
